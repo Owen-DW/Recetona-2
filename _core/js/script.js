192c7ca9231f9c4bb79eb0100812735da8bfb025
Owen-DW <steinleowen@gmail.com>
2024-10-30 21:49:09 -0300

    see agrego todos los js respectivos a los html para trabajar en la consigna 2

---
+++ _core/js/script.js
@@ -0,0 +1,17 @@
+
+
+// CONSIGNA 1
+let imagenAnimacion=document.getElementById("categoria00-item01");
+
+imagenAnimacion.addEventListener("click", function(){
+    alert("CLICKEASTE SOBRE  LA IMAGEN!") 
+    imagenAnimacion.style.backgroundColor= "blue";
+}
+)
+
+imagenAnimacion.addEventListener("mouseover", function(){
+    imagenAnimacion.style.backgroundColor= "green";
+})
+
+
+//CONSIGNA 2
