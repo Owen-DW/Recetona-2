974bf560e86b3c8f81b3f8c4aa77e708acdcec0b
Owen-DW <steinleowen@gmail.com>
2024-11-08 00:42:14 -0300

    RECETONA TERMINADA 100%

--- _core/js/script.js
+++ _core/js/script.js
@@ -1,16 +1,22 @@
 // CONSIGNA 1
-let imagenAnimacion = document.getElementsByClassName('articulo-categoria');
+document.addEventListener("DOMContentLoaded", function() {
 
-for (let i = 0; i <= imagenAnimacion.length; i++) {
+    let imagenAnimacion = document.getElementsByClassName('articulo-categoria');
+    for (let i = 0; i <= imagenAnimacion.length; i++) {
+        imagenAnimacion[i].addEventListener("click", function() {
+            alert("¡CLICKASTE SOBRE LA IMAGEN!");
+        });
+        imagenAnimacion[i].addEventListener("mouseover", function() {
+            imagenAnimacion[i].style.backgroundColor = "#5e030f";
+        });
+        imagenAnimacion[i].addEventListener("mouseout", function() {
+            this.style.backgroundColor = "#5e030f9f"; // Restablece el color original
+        });
+    }
+});
 
-    imagenAnimacion[i].addEventListener("click", function() {
-        alert("CLICKEASTE SOBRE  LA IMAGEN!");
-    });
 
-    imagenAnimacion[i].addEventListener("mouseover", function() {
-        imagenAnimacion[i].style.backgroundColor = "#5e030f";
-    });
-    imagenAnimacion[i].addEventListener("mouseout", function() {
-        this.style.backgroundColor = "#5e030f9f"; // Restablece el color original
-    });
-}
+
+
+
+//CONSIGNA 2
